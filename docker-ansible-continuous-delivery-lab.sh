6e8196918edf6b3f7ee2bdb5eadfa3460c38695e
Chris Malone <surefoot@gmail.com>
2016-07-22 23:46:47 -0400

    Remove sudo from lab clone

--- docker-ansible-continuous-delivery-lab.sh
+++ docker-ansible-continuous-delivery-lab.sh
@@ -3,5 +3,5 @@
 # pull down repos for https://app.pluralsight.com/library/courses/docker-ansible-continuous-delivery class
 
 for repo in docker-jenkins docker-ansible todobackend-base todobackend todobackend-specs todobackend-deploy todobackend-client ; do
-  sudo -iu vagrant git clone https://github.com/jmenga/$repo.git develop/$repo
+  git clone https://github.com/jmenga/$repo.git develop/$repo
 done
